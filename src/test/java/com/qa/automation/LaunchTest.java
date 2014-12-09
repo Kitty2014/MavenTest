627b888c333c92800f0fbc8787efa66b2bc8ff53
srikanth <srikanth@shopkick.com>
2014-12-09 06:30:38 -0800

    Added Build Paramters

--- src/test/java/com/qa/automation/LaunchTest.java
+++ src/test/java/com/qa/automation/LaunchTest.java
@@ -1,6 +1,12 @@
 package com.qa.automation;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSDriver;
 import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 /**
  * Created by Srikanth on 12/5/14.
@@ -8,8 +14,16 @@ import org.junit.Test;
 public class LaunchTest {
 
     @Test
-    public void testCommand(){
-        System.out.println(System.getProperty("platformType"));
-        System.out.println(System.getProperty("platformVersion"));
+    public void testCommand() throws Exception{
+        //System.out.println(System.getProperty("platformType"));
+        //System.out.println(System.getProperty("platformVersion"));
+        DesiredCapabilities capabilities=new DesiredCapabilities();
+        capabilities.setCapability("platformName",System.getProperty("platformType"));
+        capabilities.setCapability("platformVersion",System.getProperty("platformVersion"));
+        capabilities.setCapability("deviceName",System.getProperty("deviceName"));
+        capabilities.setCapability("udid",System.getProperty("deviceUDID"));
+        capabilities.setCapability("bundleId","com.shopkick.debug-qa");
+        AppiumDriver driver= new IOSDriver(new URL("http://0.0.0.0:4723/wd/hub"),capabilities);
+
     }
 }
