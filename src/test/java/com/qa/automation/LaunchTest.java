38c6b7312ab09e44c9904c1de6c3f6d387bd584c
srikanth <srikanth@shopkick.com>
2014-12-09 09:10:36 -0800

    Added Test Steps

--- src/test/java/com/qa/automation/LaunchTest.java
+++ src/test/java/com/qa/automation/LaunchTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Srikanth on 12/5/14.
@@ -15,8 +16,6 @@ public class LaunchTest {
 
     @Test
     public void testCommand() throws Exception{
-        //System.out.println(System.getProperty("platformType"));
-        //System.out.println(System.getProperty("platformVersion"));
         DesiredCapabilities capabilities=new DesiredCapabilities();
         capabilities.setCapability("platformName",System.getProperty("platformType"));
         capabilities.setCapability("platformVersion",System.getProperty("platformVersion"));
@@ -24,6 +23,24 @@ public class LaunchTest {
         capabilities.setCapability("udid",System.getProperty("deviceUDID"));
         capabilities.setCapability("bundleId","com.shopkick.debug-qa");
         AppiumDriver driver= new IOSDriver(new URL("http://0.0.0.0:4723/wd/hub"),capabilities);
-
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.findElementsByClassName("UIAButton").get(2).click(); //Next on Deals
+        driver.findElementsByClassName("UIAButton").get(3).click(); //Select Country USA
+        driver.findElementsByClassName("UIAButton").get(4).click(); //Next on Country Picker
+        driver.findElementsByClassName("UIAButton").get(3).click(); //Next on Walkin
+        driver.findElementsByClassName("UIAButton").get(2).click(); //Next on Rewards
+        driver.findElementsByClassName("UIAButton").get(2).click(); //Next on Rewards Picker
+        driver.findElementsByClassName("UIAButton").get(3).click(); //Signup on LoginFlow
+        driver.findElementsByClassName("UIAButton").get(4).click(); //Click on Email Login
+        driver.findElementsByClassName("UIATextField").get(0).sendKeys("srikanth.vejendla@yahoo.com"); //Set UserName
+        Thread.sleep(2000);
+        driver.findElementsByClassName("UIASecureTextField").get(0).sendKeys("hdxjhdjh"); //Set Password
+        driver.findElementsByClassName("UIAButton").get(3).click();
+        Thread.sleep(2000);
+        //driver.findElement(By.xpath("//UIAApplication[1]/UIAWindow[3]/UIAKeyboard[1]/UIAButton[4]")).click();
+        driver.findElementsByClassName("UIAButton").get(1).click();
+        Thread.sleep(5000);
+        driver.findElementsByClassName("UIAStaticText").get(0).isDisplayed();
+        driver.quit();
     }
 }
